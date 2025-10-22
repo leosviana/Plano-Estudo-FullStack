1c76e40a4eef6a5407cde572315dd80fa95beca7
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-10-22 16:40:13 -0300

    atual. doc

---
+++ JS+ARMAZENAMENTO/Dia002/js/funcoes.js
@@ -0,0 +1,10 @@
+//Exportando uma função normal
+export function somar(a, b){
+    return a + b;
+}
+
+//exportando uma arrow function
+export const dobrar = x => x * 2;
+
+//exportando uma constante
+export const versao = "1.0.0";
---
+++ JS+ARMAZENAMENTO/Dia002/js/main.js
@@ -0,0 +1,6 @@
+//Importando funções, variáveis do arquivo funcoes.js
+import {somar, dobrar, versao} from "./funcoes.js";
+
+console.log(somar(2, 3)); //5
+console.log(dobrar(4));   //8
+console.log(versao);      //1.0.0
