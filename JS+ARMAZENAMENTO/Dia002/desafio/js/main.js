841d776e92a3634471d0dc8305b7886f02d53a12
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-10-24 17:06:47 -0300

    atualizando doc

--- JS+ARMAZENAMENTO/Dia002/desafio/js/main.js
+++ JS+ARMAZENAMENTO/Dia002/desafio/js/main.js
@@ -1,5 +1,16 @@
 import {funcoes} from "./funcoes.js"
 import {alunos} from "./alunos.js"
 
-const aluno = new Alunos("Joao");
-console.log(aluno.media());
+const aluno = [
+    new Alunos("Ana", [8, 7.5, 9]),
+    new Alunos("João", [6, 5.5, 6.5]),
+    new Alunos("Pedro", [10, 9.5, 9])
+];
+
+//Percorre cada aluno e mostra o resultado
+aluno.forEarch(aluno => {
+    const media = aluno.media();
+    const status = verificarAprovacao(media);
+
+    console.log(`${aluno.nome}: Média = ${formatarNota(media)} -> ${status}`);
+});
