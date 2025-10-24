9dd1aa69dd95f1dbc7d1ca100fc8038571c47fd3
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-10-24 17:23:08 -0300

    atual doc

--- JS+ARMAZENAMENTO/Dia002/desafio/js/main.js
+++ JS+ARMAZENAMENTO/Dia002/desafio/js/main.js
@@ -1,10 +1,10 @@
 import {verificarAprovacao, formatarNota} from "./funcoes.js";
-import Alunos from "./alunos.js";
+import Aluno from "./alunos.js";
 
 const alunos = [
-    new Alunos("Ana", [8, 7.5, 9]),
-    new Alunos("João", [6, 5.5, 6.5]),
-    new Alunos("Pedro", [10, 9.5, 9])
+    new Aluno("Ana", [8, 7.5, 9]),
+    new Aluno("João", [6, 5.5, 6.5]),
+    new Aluno("Pedro", [10, 9.5, 9])
 ];
 
 //Percorre cada aluno e mostra o resultado
