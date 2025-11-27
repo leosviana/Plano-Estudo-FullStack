6e327eb36ca2f4e3130bbf1affcee4be1e9b7b51
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-11-27 10:59:23 -0300

    alterando doc

--- JS+ARMAZENAMENTO/Dia003/desafio/js/main.js
+++ JS+ARMAZENAMENTO/Dia003/desafio/js/main.js
@@ -1,11 +1,11 @@
-import Canal from "./canal.js";
+import Canal from "./Canal.js";
 
-const canal = new Canal("Leo Dev", 12400);
+const canal = new Canal("Leonardo Dev", 12400);
 
 canal
-    .adicionarVideo("Como criar API", 52000, 8000, "2023-11-01");
-    .adicionarVideo("HTML para iniciantes", 48000, 6000, "2023-09-15");
-    .adicionarVideo("CSS Flexbox completo", 30500, 3300, "2023-10-01");
-    .listarVideos();    
-    .estatisticas();
-    .buscarVideoPorTermo("api");
+  .adicionarVideo("Como criar API", 52000, 8000, "2023-11-10")
+  .adicionarVideo("HTML para iniciantes", 48000, 6000, "2023-09-15")
+  .adicionarVideo("CSS Flexbox completo", 30500, 3300, "2023-10-01")
+  .listarVideos()
+  .estatisticas()
+  .buscarVideosPorTermo("api");
